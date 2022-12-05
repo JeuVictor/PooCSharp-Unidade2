a5fbd38ba6e980c95d7c372ebff3af15b1b37481
Jeú Victor <91163835+JeuVictor@users.noreply.github.com>
2022-12-04 22:59:42 -0300

    Add files via upload

---
+++ Atidade1/Carros.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atidade1
+{
+    internal class Carros
+    {
+		private string Placa;
+
+		public string placa
+		{
+			get { return Placa; }
+			set { Placa = value; }
+		}
+		private string Cor;
+
+		public string cor
+		{
+			get { return Cor; }
+			set { Cor = value; }
+		}
+		private string RENAVAM;
+
+		public string renavam
+		{
+			get { return RENAVAM; }
+			set { RENAVAM = value; }
+		}
+		private string Modelo;
+
+		public string modelo
+		{
+			get { return Modelo; }
+			set { Modelo = value; }
+		}
+		private string Marca;
+
+		public string MyProperty
+		{
+			get { return Marca; }
+			set { Marca = value; }
+		}
+		public int indice;
+		private string ACAO;
+
+		public string acao
+		{
+			get { return ACAO; }
+			set { ACAO = value; }
+		}
+		public int Ativ;
+		public bool interrupitor= true;
+		public string parada;
+
+		public void Dados()
+		{
+            Console.WriteLine("Digite a COR do "+indice+"° carro: " );
+            this.Cor = Console.ReadLine();
+            Console.WriteLine("Digite RENAVAM do " + indice + "° carro: ");
+            this.RENAVAM = Console.ReadLine(); 
+            Console.WriteLine("Digite a PLACA do " + indice + "° carro: ");
+            this.Placa = Console.ReadLine();
+            Console.WriteLine("Digite O MODELO do " + indice + "° carro: ");
+            this.Modelo = Console.ReadLine();
+            Console.WriteLine("Digite a MARCA do " + indice + "° carro: ");
+            this.Marca = Console.ReadLine();
+        }
+
+		public void CarroX()
+		{
+			
+			Console.WriteLine("Marca: " + Marca);
+            Console.WriteLine("Modelo: " + modelo);
+            Console.WriteLine("Placa:" + placa);
+            Console.WriteLine("RENAVAM: " + renavam);
+            Console.WriteLine("Cor: "+cor);
+			Console.WriteLine("____________________________________");
+			Console.WriteLine("");
+        }
+		public void Acelerar()
+		{
+			
+			ACAO = ACAO.ToUpper();
+			if (acao =="ACELERAR")
+			{
+				Console.WriteLine("O carro está acelerando");
+			}
+
+		}
+		public void Frear()
+		{
+            acao = acao.ToUpper();
+            if (acao == "FREAR")
+            {
+                Console.WriteLine("O carro está freando");
+            }
+			
+        }
+
+
+
+
+	}
+}
---
+++ Atidade1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atidade1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Questão 1: Crie uma classe de nome Carro e atribua a ela todas as propriedades que você acredita que um carro possua. Instancie a classe Carro e preencha 3 objetos distintos. \r\nImprima na tela do usuário todos os atributos dos três carros.\r\n");
+            Console.WriteLine("");
+            Console.WriteLine("Questão 2: Pesquise como um criar um método específico dentro de uma classe em C#. Crie dois métodos para a classe Carro criada na questão anterior. Um método chamado Acelerar(), que retorna a string “O carro está em movimento” como resposta e outro método Freiar() que também retorna uma string “O carro está parado” como resposta.\r\n");
+            Console.WriteLine("");
+            Console.WriteLine("Digite se gostaria de ir para atividade 1 ou 2");
+            Carros Atividade = new Carros();
+            Atividade.Ativ = int.Parse(Console.ReadLine());
+            if (Atividade.Ativ == 1)
+            {
+                Carros carro1 = new Carros();
+                carro1.indice = 1;
+                carro1.Dados();
+
+                Carros carro2 = new Carros();
+                carro2.indice = 2;
+                carro2.Dados();
+
+                Carros carro3 = new Carros();
+                carro3.indice = 3;
+                carro3.Dados();
+
+                Console.Clear();
+                carro1.CarroX();
+                carro2.CarroX();
+                carro3.CarroX();
+                Console.ReadKey();
+            }
+           
+            else
+            { while (Atividade.interrupitor == true)
+                {
+                    Console.WriteLine("Digite 'ACELERAR' para acelerar o carro ou 'FREAR' para frear: ");
+                    Atividade.acao = Console.ReadLine();
+                    Atividade.Acelerar();
+                    Atividade.Frear();
+                    Console.WriteLine("Deseja encerrar? Se sim digite 'SAIR' ");
+                    Atividade.parada = Console.ReadLine();
+                    Atividade.parada = Atividade.parada.ToUpper();
+                    if (Atividade.parada == "SAIR")
+                    { Atividade.interrupitor = false; }
+                    else { Console.Clear(); }
+                }
+               
+                
+                
+            }
+           
+        }
+    }
+}
